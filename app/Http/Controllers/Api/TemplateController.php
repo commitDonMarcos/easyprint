da6a7d0a81c2916769046bdb591e8e2bf8fd4c82
Don <don@easyprint.com>
2026-06-01 12:23:00 +0800

    Add error logging to TemplateController

--- app/Http/Controllers/Api/TemplateController.php
+++ app/Http/Controllers/Api/TemplateController.php
@@ -6,12 +6,22 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\TemplateResource;
 use App\Models\Template;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Log;
 
 class TemplateController extends Controller
 {
     public function index(): AnonymousResourceCollection
     {
-        return TemplateResource::collection(Template::all());
+        try {
+            return TemplateResource::collection(Template::all());
+        } catch (\Throwable $e) {
+            Log::error('Template index error: ' . $e->getMessage(), [
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            throw $e;
+        }
     }
 
     public function show(Template $template): TemplateResource
